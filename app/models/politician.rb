4032db942c4bd4c9dbfc9610c8655c9d6668374d
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-04 20:11:36 +0000

    added politicians relations

--- app/models/politician.rb
+++ app/models/politician.rb
@@ -1,4 +1,9 @@
 class Politician < ApplicationRecord
+  has_many :reviews
+  has_many :meetings
+  has_many :votes
+
+  scope :top_rated, -> { joins(:reviews).group("politicians.id").order("AVG(SUM(reviews.score)) DESC")}
   validates :name,  presence: true
   validates :tag_line, presence: true
   validates :photo_url, presence: true
