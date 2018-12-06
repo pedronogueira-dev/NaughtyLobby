0a2605c861c6f634b9ec413c1a0402e8334a7505
Jami Owen <jamirene88@gmail.com>
2018-12-06 18:11:54 +0000

    #2 #5 #9 implemented vote creation and deletion along with validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def dashboard
     @reviews = current_user.reviews
     @meetings = current_user.meetings
-    @votes = current_user.meetings
+    @votes = current_user.votes
   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,34 @@
 class VotesController < ApplicationController
   def create
+    @vote = Vote.new(vote_params)
+
+    @politician = Politician.find(params[:politician_id])
+
+    @vote.politician = @politician
+    @vote.user = current_user
+
+    if @vote.save
+      flash[:notice] = "You have submited the information successfully!"
+      redirect_to :dashboard
+    else
+      flash[:alert] = "You already bought this vote!"
+      redirect_to politician_path(@politician)
+    end
   end
 
   def destroy
+    @vote = Vote.find(params[:id])
+    if current_user == @vote.user
+      @vote.destroy
+    else
+      flash[:alert] = "You do not have permission to delete this vote!"
+    end
+    redirect_to :dashboard
+  end
+
+  private
+
+  def vote_params
+    params.require(:vote).permit(:position, :subject)
   end
 end
