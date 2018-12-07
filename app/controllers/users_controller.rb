1156cc994acde32a962dc01acd7366202ec64f68
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-07 09:04:37 +0000

    uploader

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,22 @@ class UsersController < ApplicationController
     @meetings = current_user.meetings
     @votes = current_user.votes
   end
+
+  def upload
+    if params[:user].present? && params[:user][:photo_url].present?
+      preloaded = Cloudinary::PreloadedFile.new(params[:user][:photo_url])
+      raise "Invalid upload signature" if !preloaded.valid?
+      @user.photo_url = preloaded.identifier
+    end
+    redirect_to :dashboard
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  private
+  def photo_params
+    params.require(:user).permit(:photo_url)
+  end
 end
