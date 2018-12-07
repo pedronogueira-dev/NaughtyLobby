1e431d8fb119543afae0c67162d63137a017da80
Thomas Singell <tsingell@gmail.com>
2018-12-07 10:17:19 +0000

    added mission index page showing top 10 values and tweets

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tweets, only: [:index]
   resources :politicians, only: [:index, :show] do
     member do
       get 'search_by_name', to: 'politicians#search_by_name'
