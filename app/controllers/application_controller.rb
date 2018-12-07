5013a28c638c57f162e5b4419cc26ec7899d88be
Pedro Nogueira <17pedro.n@gmail.com>
2018-12-07 16:18:51 +0000

    configured sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, if: :devise_controller?
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: [:home, :politicians]
 
   def after_sign_in_path_for(resource)
     dashboard_path
+  end
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
